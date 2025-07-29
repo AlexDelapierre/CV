1baa965583c76b2a06611d685ba0b33001de1452
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-07-29 21:25:23 +0200

    update CV prod

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*
-function checkViewport() {
-  const mobileBlock = document.getElementById('mobile-block');
-  const desktopBlock = document.getElementById('desktop-block');
-  
-  if (window.innerWidth <= 768) {
-    mobileBlock.style.display = 'block';
-    desktopBlock.style.display = 'none';
-  } else {
-    mobileBlock.style.display = 'none';
-    desktopBlock.style.display = 'block';
-  }
-}
-*/  
-
 function checkViewport() {
   const mobileBlocks = document.querySelectorAll('.mobile-block');
   const desktopBlocks = document.querySelectorAll('.desktop-block');
@@ -30,16 +15,6 @@ function checkViewport() {
 window.addEventListener('load', checkViewport);
 // Vérifie à chaque redimensionnement
 window.addEventListener('resize', checkViewport);
-
-/*
-// Supprime #others-experiences juste avant l'impression
-window.addEventListener('beforeprint', () => {
-  const othersExperiences = document.getElementById('others-experiences');
-  if (othersExperiences) {
-    othersExperiences.remove(); // Supprime définitivement l'élément du DOM
-  }
-});
-*/
 
 // Cacher plusieurs blocs avant impression
 window.addEventListener('beforeprint', () => {
