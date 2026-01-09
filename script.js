dd6a9c7a51f2ecebf5b8024f2d194d5b9b876f27
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-01-09 19:45:51 +0100

    CV update

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 function checkViewport() {
-  const mobileBlocks = document.querySelectorAll('.mobile-block');
-  const desktopBlocks = document.querySelectorAll('.desktop-block');
+  const mobileBlocks = document.querySelectorAll(".mobile-block");
+  const desktopBlocks = document.querySelectorAll(".desktop-block");
 
   if (window.innerWidth <= 1023) {
-    mobileBlocks.forEach(el => el.style.display = 'block');
-    desktopBlocks.forEach(el => el.style.display = 'none');
+    mobileBlocks.forEach((el) => (el.style.display = "block"));
+    desktopBlocks.forEach((el) => (el.style.display = "none"));
   } else {
-    mobileBlocks.forEach(el => el.style.display = 'none');
-    desktopBlocks.forEach(el => el.style.display = 'block');
+    mobileBlocks.forEach((el) => (el.style.display = "none"));
+    desktopBlocks.forEach((el) => (el.style.display = "block"));
   }
 }
 
 // Vérifie au chargement
-window.addEventListener('load', checkViewport);
+window.addEventListener("load", checkViewport);
 // Vérifie à chaque redimensionnement
-window.addEventListener('resize', checkViewport);
+window.addEventListener("resize", checkViewport);
 
 // Cacher plusieurs blocs avant impression
-window.addEventListener('beforeprint', () => {
-  const idsToHide = ['projets', 'formations', 'last-article'];
-  const printBlocks = document.querySelectorAll('.print-block');
+// window.addEventListener('beforeprint', () => {
+//   const idsToHide = ['projets', 'formations', 'last-article'];
+//   const printBlocks = document.querySelectorAll('.print-block');
 
-  idsToHide.forEach(id => {
-    const el = document.getElementById(id);
-    if (el) el.style.display = 'none';
-  });
-  printBlocks.forEach(el => el.style.display = 'block');
-});
+//   idsToHide.forEach(id => {
+//     const el = document.getElementById(id);
+//     if (el) el.style.display = 'none';
+//   });
+//   printBlocks.forEach(el => el.style.display = 'block');
+// });
 
 // Réafficher les blocs après impression
-window.addEventListener('afterprint', () => {
-  const idsToHide = ['languages', 'projets', 'formations', 'soft-skills'];
-  const printBlocks = document.querySelectorAll('.print-block');
+// window.addEventListener('afterprint', () => {
+//   const idsToHide = ['languages', 'projets', 'formations', 'soft-skills'];
+//   const printBlocks = document.querySelectorAll('.print-block');
 
-  idsToHide.forEach(id => {
-    const el = document.getElementById(id);
-    if (el) el.style.display = '';
-  });
-  printBlocks.forEach(el => el.style.display = 'none');
-});
-
+//   idsToHide.forEach(id => {
+//     const el = document.getElementById(id);
+//     if (el) el.style.display = '';
+//   });
+//   printBlocks.forEach(el => el.style.display = 'none');
+// });
